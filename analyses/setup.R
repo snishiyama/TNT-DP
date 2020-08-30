be243b6ba7e11012918b7367c1269495dbf564d7
Satoru Nishiyama <nishi.satoru.yama0226@gmail.com>
2020-08-30 23:57:41 +0900

    make functions for multiple comparisons

--- analyses/setup.R
+++ analyses/setup.R
@@ -2,6 +2,31 @@ library(tidyverse)
 library(rstatix)
 library(here)
 
+multi_aov <- function(df, factor, model, levels) {
+  aov_2levels <- function(df, factor, model, level1, level2) {
+    df %>% 
+      dplyr::filter(!!rlang::sym(factor) %in% c(level1, level2)) %>% 
+      rstatix::anova_test(model) %>% 
+      rstatix::get_anova_table(correction = "auto") %>% 
+      as_tibble() %>% 
+      dplyr::filter(Effect == factor) %>% 
+      dplyr::mutate(level1 = level1, level2 = level2)
+  }
+
+  contrasts <- combn(levels, m = 2)  %>% 
+    split(col(.))
+
+  res <- contrasts %>% 
+    purrr::map(~aov_2levels(df, factor, model, .x[1], .x[2]))%>% 
+    dplyr::bind_rows() %>% 
+    dplyr::select(level1, level2, DFn:p, ges) %>% 
+    rstatix::adjust_pvalue(p.col = "p", method = "holm") %>% 
+    dplyr::mutate(t = sqrt(`F`))
+
+  return(res)
+}
+
+
 df_e1 <- readr::read_csv(here::here("exp/data/sotsuron_exp1_part2.csv")) %>% 
   dplyr::select(-1)
 
@@ -14,10 +39,28 @@ df_recall_e1 <- df_e1 %>%
   tidyr::pivot_longer(think:substitute, names_to = "status", values_to = "rate")
 
 # anova t vs nt vs b
+model_recall_e1 <- rate ~ suppression*status + Error(participant/status)
 aov_recall_e1 <- df_recall_e1 %>% 
   dplyr::filter(status != "substitute") %>% 
-  rstatix::anova_test(rate ~ suppression*status + Error(participant/status))  %>% 
+  rstatix::anova_test(model_recall_e1) %>% 
   rstatix::get_anova_table(correction = "auto")
+
+
+# multiple comparisons
+multi_aov(df_recall_e1, "status", model_recall_e1, c("think", "nothink", "baseline"))
+
+# multiple comparisons with emmeans
+df_recall_e1 %>% 
+  dplyr::filter(status != "substitute") %>% 
+  dplyr::mutate(participant = as_factor(participant),
+                suppression = as_factor(suppression),
+                status = factor(status, levels = c("think", "nothink", "baseline"))) %>% 
+  aov(rate ~ suppression*status+Error(participant/status), 
+      contrasts = list(suppression = "contr.sum", status = "contr.sum"), # type 3 SS
+      data = .) %>% 
+  emmeans::emmeans(~ status) %>% 
+  emmeans::contrast(method = "pairwise", adjust = "holm")
+  
 
 pwt_recall_e1 <- df_recall_e1 %>% 
   dplyr::filter(status != "substitute") %>% 
@@ -26,43 +69,12 @@ pwt_recall_e1 <- df_recall_e1 %>%
 rate <- df_recall_e1 %>% 
   dplyr::filter(status != "substitute") %>% 
   dplyr::pull(rate)
-
-df_recall_e1 %>% 
-  dplyr::filter(status != "substitute") %>% 
-  rstatix::emmeans_test(rate ~ status, p.adjust.method = "holm", detailed = T)
   
-source(here::here("analyses/anovakun_485.txt"))
-aovkun <- df_recall_e1 %>% 
-  dplyr::filter(status != "substitute") %>% 
-  anovakun("AsB", long = T, geta = T)
+# source(here::here("analyses/anovakun_485.txt"))
+# aovkun <- df_recall_e1 %>%
+#   dplyr::filter(status != "substitute") %>%
+#   anovakun("AsB", long = T, geta = T)
 
-# 0.0898/sqrt(0.0050*(2/36))
-# 
-# 0.898 / 4.4901
-
-# temp2 <- df_recall_e1 %>% 
-#   dplyr::filter(!status %in% c("substitute", "think")) %>% 
-#   tidyr::pivot_wider(names_from = status, values_from = rate) %>% 
-#   dplyr::mutate(diff = nothink - baseline) %>% 
-#   dplyr::summarise(mean = mean(diff), sd = sd(diff), se = sd/sqrt(36), t = mean/se)
-# 
-# temp <- df_recall_e1 %>% 
-#   dplyr::filter(!status %in% c("substitute", "think")) %>% 
-#   rstatix::anova_test(rate ~ status + Error(participant/status), detailed = T)
-# 
-# temp <- df_recall_e1 %>% 
-#   dplyr::filter(!status %in% c("substitute", "think")) %>% 
-#   dplyr::mutate(participant = as_factor(participant)) %>% 
-#   lm(rate ~ status + participant, data = .) %>% 
-#   car::Anova(type = 3)
-# 
-# bon.SE <- sqrt((bon.denomi[comb.frame[1, ]] + bon.denomi[comb.frame[2, ]]) * bon.Ve)
-# bon.t <- abs(bon.delta / bon.SE)# ｔ値は絶対値を取る
-# bon.p <- pt(bon.t, bon.df, lower.tail = FALSE) * 2# 両側確率
-# cont.N <- table(dat[, 2:nchar(design)])# 各セルのデータ数
-# bon.denomi <- apply(1/cont.N, bon.num, mean) / (prod(factlevels)/2)# セルごとに重み付けしたデータ数の平均を残りの条件数で割ったもの
-# othink-baseline  -0.0760   4.3749  34  0.0001  0.0002  nothink < baseline *
-  # (aovkun$`POST ANALYSES`$B$bontab$difference[2] / aovkun$`POST ANALYSES`$B$bontab$t[2])^2 / (temp$`Sum Sq`[4] / 35)
 # calculate effect size
 # pipeR::pipeline({
 #   exp1_res_anova_rate$`POST ANALYSES`$B$bontab
@@ -86,14 +98,15 @@ df_ques_e1 <- df_e1 %>%
                       names_sep = "_",
                       values_to = "rating")
 
+model_ques_e1 <- rating ~ suppression*type+Error(participant/type)
 l_aov_ques_e1 <- df_ques_e1 %>% 
   split(.$condition) %>% 
-  purrr::map(.f = rstatix::anova_test, rating ~ suppression*type+Error(participant/type)) %>% 
+  purrr::map(.f = rstatix::anova_test, model_ques_e1) %>% 
   purrr::map(.f = rstatix::get_anova_table, correction = "auto")
 
-pwt_ques_think_e1 <- df_ques_e1 %>% 
+mc_ques_think_e1 <- df_ques_e1 %>% 
   dplyr::filter(condition == "think") %>% 
-  rstatix::pairwise_t_test(rating ~ type, paired = T)
+  multi_aov("type", model_ques_e1, c("cue", "tar", "sub"))
 
 sme_ques_nt_e1 <- df_ques_e1 %>% 
   dplyr::filter(condition == "nothink") %>% 
